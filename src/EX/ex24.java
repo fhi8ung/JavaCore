a7310c3217e9a738d7e77a2e2003e7bb983c9d87
Nguyễn Phi Hùng <fhihung@hotmail.com>
2020-04-07 21:19:41 +0700

    Create ex24.java

---
+++ src/EX/ex24.java
@@ -0,0 +1,31 @@
+package EX;
+
+import java.util.Scanner;
+
+public class ex24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào chiều dài số Fibonacci: ");
+        int x = sc.nextInt();
+        System.out.println(x + " các số Fibonaci đầu tiên:");
+
+        String fib = "";
+        if (x == 1) {
+            fib = "0";
+        } else if (x == 2) {
+            fib = "0, 1";
+        } else {
+            int U1 = 0;
+            int U2 = 1;
+            fib = U1 + ", " + U2;
+
+            for (int i = 3; i <= x; i++) {
+                int Un = U1 + U2;
+                fib = fib + ", " + Un;
+                U1 = U2;
+                U2 = Un;
+            }
+        }
+        System.out.println(fib);
+    }
+}
