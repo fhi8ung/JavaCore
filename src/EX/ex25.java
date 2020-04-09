35f3003e44b82f910e50b83eb49eb549bfac127d
Nguyễn Phi Hùng <fhihung@hotmail.com>
2020-04-09 20:17:13 +0700

    done ex25

--- src/EX/ex25.java
+++ src/EX/ex25.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class ex25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sout 
+        System.out.println("Nhập vào n: ");
+        int n = sc.nextInt();
+        int tong = 0;
+        for(int i = 0; i <= n; i++ ) {
+            tong = tong + i;
+        }
+        System.out.println("Tong là " + tong);
+        float trungbinhcong = tong/n;
+        System.out.println("Trung bình cộng là: " + trungbinhcong);
     }
 }
