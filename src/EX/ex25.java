9134c15c4327009a6b5bb6efd04790766e2e6c2e
Nguyễn Phi Hùng <fhihung@hotmail.com>
2020-04-09 19:52:31 +0700

    ex25

--- src/EX/ex25.java
+++ src/EX/ex25.java
@@ -1,5 +1,10 @@
 package EX;
 
-public class ex25 {
+import java.util.Scanner;
 
+public class ex25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sout 
+    }
 }
