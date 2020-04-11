448effbe4be99c853ee78cffbb54da495c79d531
Nguyễn Phi Hùng <fhihung@hotmail.com>
2020-04-11 14:40:56 +0700

    done ex28

---
+++ src/EX/ex28.java
@@ -0,0 +1,14 @@
+package EX;
+
+import java.util.Scanner;
+
+public class ex28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào số đầu: ");
+        float sodau = sc.nextFloat();
+        System.out.println("Nhập vào số thứ hai: ");
+        float sothuhai = sc.nextFloat();
+        System.out.println("Kết quả là: " +sodau*sothuhai);
+    }
+}
