deef041170ad90974201bd1d5d29be37e185ad2d
Nguyễn Phi Hùng <fhihung@hotmail.com>
2020-04-21 22:10:34 +0700

    up

--- src/EX3/ex35.java
+++ src/EX3/ex35.java
@@ -1,4 +1,21 @@
 package EX3;
 
+import java.util.Scanner;
+
 public class ex35 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào số phần tử: ");
+        int n = sc.nextInt();
+        int [] array = new int[n];
+        for (int i = 0; i < n; i++ ){
+            System.out.println("Nhập vào phần tử thứ: " +(i+1)+" ");
+            array [i] = sc.nextInt();
+        }
+        System.out.println("Mảng vừa nhập là: ");
+        for (int i = 0; i < n; i++){
+            System.out.print(array[i]+ " " );
+        }
+        
+    }
 }
